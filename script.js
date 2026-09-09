ffaef4f7a6bcf755089004861c892c0ade5c3bf4
Abdurrahman <reald1no@icloud.com>
2026-09-09 22:49:04 +0300

    js updated

--- script.js
+++ script.js
@@ -36,5 +36,65 @@
 
     const recolor = document.querySelector('#recolor-button');
     recolor.addEventListener('click', () => {
-    recolor.classList.toggle('#recolor-button')
+    recolor.classList.toggle('active')
     })
+    
+    const sum =(a,b) =>{
+      const sum = a + b;
+      return sum * sum
+    };
+    console.log(sum(10,20))
+
+  const age = 17
+  if (age>=18) {
+    console.log('доступ разрешен')
+  } else {
+    console.log('доступ запрещен')
+  }
+
+let temprerature = 20
+if (temprerature>20){
+  console.log('холодно')}
+  else if (temprerature===20){
+    console.log('температура равна 20')
+  }
+
+  function weather (city,temprerature)
+  { 
+    console.log(`В городе ${city} сейчас ${temprerature} градусов`)
+  }
+  weather('москва',20)
+
+  const speedLight = 300000; 
+  function speed(speed) {
+  if (speed>speedLight) 
+  {console.log('Сверхсветовая скорость' );}
+  else if (speed===speedLight){
+    console.log('скорость света');
+  }
+  else {
+    console.log('Субсветовая скорость');
+  }
+  }
+  speed(300000);
+
+  const productName = "telephone";
+  const price = 300;
+  
+  function buyProduct(balans) {
+  if (balans>=price)
+  {console.log(`вы приобрели ${productName} спасибо за покупку `)}
+  else {console.log(`вам не хватает ${price - balans} $ пополните баланс`)}
+
+  }
+  buyProduct(300);
+  
+  const userName = 'Али';
+  const userAge = 17; 
+  
+  function сheckCinemaAccess(age){
+    if (age>=18) {
+    console.log(`${userName} может купить билет цена 500р`)}
+    else {console.log(`${userName} не может купить билет ему нет 18 лет`)}
+  }
+  сheckCinemaAccess(userAge);
